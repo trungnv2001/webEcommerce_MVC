79728c981423575f201a6f38db0bed3f5b80967e
Trung <trungdaica118@gmail.com>
2024-06-04 19:31:58 +0700

    Add project files.

---
+++ webEcommerce_book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webEcommerce_book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
